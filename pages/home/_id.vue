7c92b5bf15a4639f2c97555231d5c88378b67e2b
Wojtek Pianka <wojtek.pianka@grupa26.pl>
2021-07-29 09:44:49 +0200

    Chapter 3

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -19,11 +19,6 @@ export default {
   head() {
     return {
       title: this.home.title,
-      script: [{
-        src: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEepxalX-CBi-x4ioucYzivZ7dHmYT3nw&libraries=places",
-        hid: "map",
-        defer: true,
-      }]
     }
   },
   data() {
@@ -32,16 +27,7 @@ export default {
     }
   },
   mounted() {
-    const mapOptions = {
-      zoom: 18,
-      center: new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng),
-      disableDefaultUI: true,
-      zoomControl: true
-    }
-    const map = new window.google.maps.Map(this.$refs.map, mapOptions)
-    const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng)
-    const marker = new window.google.maps.Marker({ position })
-    marker.setMap(map)
+    this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
   },
   created() {
     const first = homes.find((home) => home.objectID ==  this.$route.params.id)
